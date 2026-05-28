25b1471cdb47910f638151c3997c62fc87a63376
jassonrgomes <jassonrgomes@gmail.com>
2026-05-29 00:59:05 +0100

    Fix database connection to use DATABASE_URL

--- src/db.js
+++ src/db.js
@@ -1,9 +1,14 @@
 import mysql from "mysql2/promise"; 
 import dotenv from "dotenv"; 
-dotenv.config(); const db = mysql.createPool({ 
-  host: process.env.DB_HOST, 
-  port: process.env.DB_PORT || 3306, 
-  user: process.env.DB_USER, 
-  password: process.env.DB_PASSWORD, 
-  database: process.env.DB_NAME, }); 
+dotenv.config();
+
+const db = process.env.DATABASE_URL 
+  ? mysql.createPool(process.env.DATABASE_URL)
+  : mysql.createPool({ 
+      host: process.env.DB_HOST || "localhost", 
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306, 
+      user: process.env.DB_USER, 
+      password: process.env.DB_password || process.env.DB_PASSWORD, 
+      database: process.env.DB_NAME,
+    }); 
 export default db;
