9648025ad0bb8c1ee8caa7b072b8b6e17f8194f9
JassonRGomes <JASSONRGOMES@GMAIL.COM>
2026-06-02 00:56:12 +0100

    Add files via upload

---
+++ frontend/dev/static/chunks/Downloads_J-Lautos_frontend_0rqeker._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_0nx1v1n._.js","static/chunks/00-2_next_dist_compiled_next-devtools_index_0xj9eyl.js","static/chunks/00-2_next_dist_compiled_react-dom_13qod_-._.js","static/chunks/00-2_next_dist_compiled_react-server-dom-turbopack_0gr8k.q._.js","static/chunks/00-2_next_dist_compiled_0fwcbih._.js","static/chunks/00-2_next_dist_client_0amp.lb._.js","static/chunks/00-2_next_dist_12lcspq._.js","static/chunks/00-2_@swc_helpers_cjs_0g_qipr._.js"],
+    source: "entry"
+});
---
+++ frontend/dev/static/chunks/Downloads_J-Lautos_frontend_src_app_details_[id]_page_tsx_0afc0h1._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/Downloads_J-Lautos_frontend_src_app_00jh.2t._.js","static/chunks/00-2_0q7bue6._.js"],
+    source: "dynamic"
+});
