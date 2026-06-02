daa726dddb0cc4702a85b573561c3cefc1fd76b9
JassonRGomes <JASSONRGOMES@GMAIL.COM>
2026-06-02 02:44:01 +0100

    Add files via upload

---
+++ frontend/.next/dev/server/chunks/ssr/0ipn_J-Lautos_frontend__next-internal_server_app_details_[id]_page_actions_13-fwty.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/Downloads/J-Lautos/frontend/.next-internal/server/app/details/[id]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=0ipn_J-Lautos_frontend__next-internal_server_app_details_%5Bid%5D_page_actions_13-fwty.js.map
